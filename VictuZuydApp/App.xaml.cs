5690ee345c47cfa04298a8c25ce9f2fe934c76f0
jouryl00 <loojoury@gmail.com>
2024-12-04 11:50:16 +0100

    Add project files.

---
+++ VictuZuydApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VictuZuydApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ VictuZuydApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VictuZuydApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
